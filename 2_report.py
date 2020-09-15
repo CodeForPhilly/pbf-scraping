f02fe2f93df35bd096192f0b01a995653becc921
Michael Chow <mc_al_github@fastmail.com>
2020-09-14 20:02:45 -0400

    update analysis

--- 2_report.py
+++ 2_report.py
@@ -7,6 +7,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: venv-pbf-scraping
+#     language: python
+#     name: venv-pbf-scraping
 # ---
 
 # + tags=["hide-input"]
@@ -30,18 +34,49 @@ conn_str = "awsathena+rest://{AWS_ACCESS_KEY_ID}:{AWS_SECRET_ACCESS_KEY}@athena.
            "{SCHEMA_NAME}?s3_staging_dir={S3_STAGING_DIR}".format(**config)
 engine = create_engine(conn_str)
 
-df = pd.read_sql("""
-SELECT filing_date, bail_status, COUNT(*) as ttl
-FROM new_criminal_filings
-GROUP BY filing_date, bail_status
-""", engine)
+ncf = pd.read_sql("""
+    SELECT *
+    FROM new_criminal_filings
+    """,
+    engine
+)
+
+ncf["filing_date"] = ncf.filing_date.astype("datetime64[ns]")
 
 # + tags=["hide-input"]
-df["filing_date"] = df.filing_date.astype("datetime64[ns]")
+status_counts = (ncf
+        .groupby(["filing_date", "bail_status"])
+        .size()
+        .reset_index(name = "ttl")
+)
 
-(ggplot(df, aes("filing_date", "ttl", color = "bail_status"))
-  + geom_point()
-  + geom_line()
+(ggplot(status_counts, aes("filing_date", "ttl", fill = "bail_status"))
+  + geom_col()
   + theme(axis_text_x = element_text(angle = 45, hjust = 1))
 )
 
+
+# + tags=["hide-input"]
+type_counts = ncf[["filing_date", "bail_type"]].value_counts().reset_index(name = "n")
+
+(
+    ggplot(type_counts, aes('filing_date', 'n', fill = 'bail_type')) +
+    geom_col() +
+    labs(title = "Docket Count by Day",
+        subtitle = "Data since 2020-02-29",
+        x = "Date",
+        y = "Docket Count",
+        fill = "Bail Type") +
+    theme(axis_text_x = element_text(angle = 45)) +
+    theme_light()
+)
+# -
+
+# ### Missing days
+
+# + tags=["hide-input"]
+data_dates = pd.date_range("2020-03-01", "today")
+
+pd.DataFrame(
+    {'date': data_dates[~data_dates.isin(status_counts.filing_date)]
+    })
