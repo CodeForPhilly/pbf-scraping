d80193757a741a2baaa06a33bd824d5daaa7e3a0
Amishi <amishi@Amishis-MacBook-Pro.local>
2020-09-22 20:41:28 -0700

    Added download.py and scrape1.py

--- download.py
+++ download.py
@@ -6,26 +6,50 @@ import time
 import os
 
 
-def download(link,docketNumber):
+def download(link1,link2,docketNumber):
     #os.system('curl "'+link+'" >> downloads\\'+docketNumber+".pdf")
-    dir="D:\\Code for Philly\\PBF\\downloads\\"
-    r_pdf = requests.get(link, headers={"User-Agent": "ParsingThing"})
-    with open(dir+docketNumber+'.pdf', 'wb') as f:
+    dir1="downloads//dockets//"
+    dir2="downloads//court//"
+    r_pdf = requests.get(link1, headers={"User-Agent": "ParsingThing"})
+    with open(dir1+docketNumber+'.pdf', 'wb') as f:
         f.write(r_pdf.content)
+    r_pdf = requests.get(link2, headers={"User-Agent": "ParsingThing"})
+    with open(dir2+docketNumber+'.pdf', 'wb') as f:
+        f.write(r_pdf.content)
+
+def fetch():
+    scrape1.main()
+    return scrape1.listOfDocketNumbers
+
+def fetch1():
+    file="august.csv"
+    f = open(file, 'r')
+    lines = f.readlines()
+    new=[]
+    for line in lines:
+        new.append(line[:-1])
+    print(new)
+    return new[504:]
 
 def main():
     #docketNumbers=["MC-51-CR-0016015-2020","MC-51-CR-0016016-2020","MC-51-CR-0016017-2020","MC-51-CR-0016018-2020"]
-    scrape1.main()
-    docketNumbers=scrape1.listOfDocketNumbers
+    #fetch1()
+    docketNumbers=fetch1()
+    print(len(docketNumbers))
     print(docketNumbers)
     fireFoxOptions = webdriver.FirefoxOptions()
     fireFoxOptions.headless=True
-    driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+    driver = webdriver.Firefox(options=fireFoxOptions)
     driver.maximize_window()
     links=[]
     count=0
     for item in docketNumbers:
         try:
+            if(count%50==0 and count>0):
+                print("\nSleeping for 600 seconds")
+                time.sleep(600)
+                print("wait complete\n")
+
             docketNumber=item.split("-")
             start=time.time()
             driver.get("https://ujsportal.pacourts.us/DocketSheets/MC.aspx")
@@ -36,24 +60,25 @@ def main():
             driver.find_element_by_id("ctl00_ctl00_ctl00_cphMain_cphDynamicContent_cphDynamicContent_docketNumberCriteriaControl_docketNumberControl_mtxtSequenceNumber").send_keys(docketNumber[3])
             driver.find_element_by_id("ctl00_ctl00_ctl00_cphMain_cphDynamicContent_cphDynamicContent_docketNumberCriteriaControl_docketNumberControl_mtxtYear").send_keys(docketNumber[4])
             driver.find_element_by_id("ctl00_ctl00_ctl00_cphMain_cphDynamicContent_cphDynamicContent_docketNumberCriteriaControl_searchCommandControl").click()
-            final_click=driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td/a")
-            hover=ActionChains(driver).move_to_element(driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/a/img")).move_to_element(final_click)
+            docketDocument=driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td/a")
+            courtSummary=driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[2]/td/table/tbody/tr/td/a")
+            hover=ActionChains(driver).move_to_element(driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/a/img")).move_to_element(docketDocument)
             hover.perform()
-            links.append(final_click.get_attribute("href"))
             count += 1
             end=time.time()
             start1=time.time()
-            download(final_click.get_attribute("href"),"-".join(docketNumber))
+            download(docketDocument.get_attribute("href"),courtSummary.get_attribute("href"),"-".join(docketNumber))
             end1=time.time()
             print(count)
             print("-".join(docketNumber))
             print("selenium time:"+str(end-start))
             print("download time:" + str(end1- start1))
-        except:
+        except Exception as e:
             print("could not download docket:"+"-".join(docketNumber))
+            print(e)
 
     driver.close()
-    print(links)
 
 if __name__=="__main__":
     main()
+    #fetch1()
