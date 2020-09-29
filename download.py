c19d653fa58c22399c82e63d14ea60414fb555d5
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2020-09-29 18:58:55 -0400

    Create List of Dicts For Docket Info

--- download.py
+++ download.py
@@ -4,32 +4,39 @@ import scrape1
 import requests
 import time
 import os
+import pandas as pd
+from funcs_parse import *
+from parse_docket import *
 
 
 def download(link1,link2,docketNumber):
     #os.system('curl "'+link+'" >> downloads\\'+docketNumber+".pdf")
-    dir1="downloads//dockets//"
-    dir2="downloads//court//"
+    dir1="./tmp/dockets/"
+    dir2="./tmp/court/"
     r_pdf = requests.get(link1, headers={"User-Agent": "ParsingThing"})
     with open(dir1+docketNumber+'.pdf', 'wb') as f:
         f.write(r_pdf.content)
+    text = scrape_pdf(dir1+docketNumber+'.pdf')
+    parse = parse_pdf(dir1+docketNumber+'.pdf', text)
+    print(parse)
     r_pdf = requests.get(link2, headers={"User-Agent": "ParsingThing"})
     with open(dir2+docketNumber+'.pdf', 'wb') as f:
         f.write(r_pdf.content)
+    return parse
 
 def fetch():
     scrape1.main()
     return scrape1.listOfDocketNumbers
 
 def fetch1():
-    file="august.csv"
+    file="august_sm.csv"
     f = open(file, 'r')
     lines = f.readlines()
     new=[]
     for line in lines:
         new.append(line[:-1])
     print(new)
-    return new[504:]
+    return new
 
 def main():
     #docketNumbers=["MC-51-CR-0016015-2020","MC-51-CR-0016016-2020","MC-51-CR-0016017-2020","MC-51-CR-0016018-2020"]
@@ -43,6 +50,7 @@ def main():
     driver.maximize_window()
     links=[]
     count=0
+    parsed_results = []
     for item in docketNumbers:
         try:
             if(count%50==0 and count>0):
@@ -67,7 +75,10 @@ def main():
             count += 1
             end=time.time()
             start1=time.time()
-            download(docketDocument.get_attribute("href"),courtSummary.get_attribute("href"),"-".join(docketNumber))
+            data = download(docketDocument.get_attribute("href"),courtSummary.get_attribute("href"),"-".join(docketNumber))
+            parsed_results.append(data)
+            print('results count: ' + str(len(parsed_results)))
+            print('results.last: ' + str(parsed_results[-1]))
             end1=time.time()
             print(count)
             print("-".join(docketNumber))
@@ -78,6 +89,8 @@ def main():
             print(e)
 
     driver.close()
+    final = pd.DataFrame(parsed_results)
+    final.to_csv('tmp/test.csv', index=False)
 
 if __name__=="__main__":
     main()
