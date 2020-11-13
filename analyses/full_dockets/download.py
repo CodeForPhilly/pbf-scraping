6585ce1bc9dbbb36316b646d604e6389cb61465e
notchia <lucia.korpas@gmail.com>
2020-11-12 23:58:42 -0500

    untested: refactoring of download.py

--- analyses/full_dockets/download.py
+++ analyses/full_dockets/download.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from sqlalchemy import create_engine
 import requests
 import time
 import os
 import sys
 import pandas as pd
-from sqlalchemy import create_engine
 import parse_docket as docket
-#import parse_court as court
+import parse_court as court
+
+DOCKET_BUFFER = 50  # number of dockets before sleep
+SLEEP_TIME = 600    # seconds
 
 
 def download(docket_link, court_link, docketNumber):
@@ -24,6 +27,8 @@ def download(docket_link, court_link, docketNumber):
     r_pdf = requests.get(docket_link, headers={"User-Agent": "ParsingThing"})
     with open(dockets_file, 'wb') as f:
         f.write(r_pdf.content)
+    
+    # TODO: replace with single docket.scrape_and_parse(dockets_file)
     text_d = docket.scrape_pdf(dockets_file)
     parse_d = docket.parse_pdf(dockets_file, text_d)
 
@@ -31,7 +36,9 @@ def download(docket_link, court_link, docketNumber):
     r_pdf = requests.get(court_link, headers={"User-Agent": "ParsingThing"})
     with open(court_file, 'wb') as f:
         f.write(r_pdf.content)
-    #parse_c = court.parse_pdf(court_file) # Need to change parse_court to accept court file
+    #parse_c = court.scrape_and_parse_pdf(court_file) # Need to change parse_court to accept court file
+    
+    # TODO: move court dict entries to docket dict and return full data
     
     return parse_d
 
@@ -60,74 +67,78 @@ def fetch_docket_numbers(aws_access_key_id, aws_secret_access_key):
     return docket_list
 
 
+def get_pdf_links(driver, docketstr):
+    ''' Get docket and court summary pdf file links from website '''
+
+    docketNumber = docketstr.split("-")
+    
+    elementBase = "ctl00_ctl00_ctl00_cphMain_cphDynamicContent_cphDynamicContent_docketNumberCriteriaControl"
+    driver.get("https://ujsportal.pacourts.us/DocketSheets/MC.aspx")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.find_element_by_id("{0}_docketNumberControl_mddlCourt".format(elementBase)).send_keys(docketNumber[0])
+    driver.find_element_by_id("{0}_docketNumberControl_mtxtCounty".format(elementBase)).send_keys(docketNumber[1])
+    driver.find_element_by_id("{0}_docketNumberControl_mddlDocketType".format(elementBase)).send_keys(docketNumber[2])
+    driver.find_element_by_id("{0}_docketNumberControl_mtxtSequenceNumber".format(elementBase)).send_keys(docketNumber[3])
+    driver.find_element_by_id("{0}_docketNumberControl_mtxtYear".format(elementBase)).send_keys(docketNumber[4])
+    driver.find_element_by_id("{0}_searchCommandControl".format(elementBase)).click()
+    docketDocument = driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td/a")
+    courtSummary = driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[2]/td/table/tbody/tr/td/a")
+    hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/a/img")).move_to_element(docketDocument)
+    hover.perform()
+
+    docketLink = docketDocument.get_attribute("href")
+    courtLink = courtSummary.get_attribute("href")
+    
+    return docketLink, courtLink
+
+
 def main():
     ''' Fetch all new docket numbers and download, parse, and save .csv for
         docket and court summary corresponding to each number '''
     
-    docketNumbers = ['MC-51-CR-0021092-2020']#fetch_docket_numbers(str(sys.argv[1]), str(sys.argv[2]))
-    print('docketNumber count: ' + str(len(docketNumbers)))
-    print(docketNumbers)
+    docketList = fetch_docket_numbers(str(sys.argv[1]), str(sys.argv[2]))
+    
+    print('{0} docket numbers found'.format(len(docketList)))
     
     # Initialize web driver
     fireFoxOptions = webdriver.FirefoxOptions()
     fireFoxOptions.headless=True
     driver = webdriver.Firefox(options=fireFoxOptions)
     driver.maximize_window()
-    
-    count = 0
-    parsed_results = []
-    for item in docketNumbers:
-        # Navigate to docket and court pdf files, then download and parse
-        try:
-            if (count%50==0 and count>0):
-                print("\nSleeping for 600 seconds... ")
-                time.sleep(600)
-                print("wait complete\n")
 
-            docketNumber = item.split("-")
-            elementBase = "ctl00_ctl00_ctl00_cphMain_cphDynamicContent_cphDynamicContent_docketNumberCriteriaControl"
-            t0 = time.time()
-            driver.get("https://ujsportal.pacourts.us/DocketSheets/MC.aspx")
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            driver.find_element_by_id("{0}_docketNumberControl_mddlCourt".format(elementBase)).send_keys(docketNumber[0])
-            driver.find_element_by_id("{0}_docketNumberControl_mtxtCounty".format(elementBase)).send_keys(docketNumber[1])
-            driver.find_element_by_id("{0}_docketNumberControl_mddlDocketType".format(elementBase)).send_keys(docketNumber[2])
-            driver.find_element_by_id("{0}_docketNumberControl_mtxtSequenceNumber".format(elementBase)).send_keys(docketNumber[3])
-            driver.find_element_by_id("{0}_docketNumberControl_mtxtYear".format(elementBase)).send_keys(docketNumber[4])
-            driver.find_element_by_id("{0}_searchCommandControl".format(elementBase)).click()
-            docketDocument = driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[1]/td/table/tbody/tr/td/a")
-            courtSummary = driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/div/table/tbody/tr[2]/td/table/tbody/tr/td/a")
-            hover = ActionChains(driver).move_to_element(driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/table/tbody/tr/td/div[2]/div/div[3]/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/a/img")).move_to_element(docketDocument)
-            hover.perform()
-            count += 1
-            t1 = time.time()
+    # Navigate to docket and court pdf files, then download and parse
+    parsedData = []
+    for i, docketstr in docketList:
+        if (i%DOCKET_BUFFER==0 and i>0):
+            print("\nSleeping for {0} seconds...".format(SLEEP_TIME))
+            time.sleep(SLEEP_TIME)
+            print("wait complete\n")
             
-            data = download(docketDocument.get_attribute("href"),courtSummary.get_attribute("href"),"-".join(docketNumber))
-            parsed_results.append(data)
-            print('results count: ' + str(len(parsed_results)))
-            print('results.last: ' + str(parsed_results[-1]))
-            t2 = time.time()
-            
-            print(count)
-            print("-".join(docketNumber))
-            print("selenium time:" + str(t1-t0))
-            print("download time:" + str(t2-t1))
+        try:
+            docketLink, courtLink = get_pdf_links(driver, docketstr)
+            data = download(docketLink, courtLink, docketstr)
+            parsedData.append(data)
             
         except Exception as e:
-            print("could not download docket:"+"-".join(docketNumber))
+            print("could not download docket {0}".docketstr)
             print(e)
 
     driver.close()
 
-    # Save docket data to .csv
-    final = pd.DataFrame(parsed_results)
+    # Save all docket and court summary data to .csv
     dirname = os.path.dirname(__file__)
     os.mkdir(os.path.join(dirname, "tmp/parsed_docket_data"))
-    filepath = os.path.join(
-        dirname, "tmp/parsed_docket_data/docket-data-" + time.strftime("%Y-%m-%d-%H%M%S") + ".csv")
+    filename = "tmp/parsed_docket_data/docket-data-{0}.csv".format(time.strftime("%Y-%m-%d-%H%M%S"))
+    filepath = os.path.join(dirname, filename)
+    
+    final = pd.DataFrame(parsedData)
     final.to_csv(filepath, index=False)
 
     return
+
+
+def test_download(testlist=[]):
+    return testlist
 
 
 if __name__=="__main__":
