af974769d9a3a2149d8918e206895f834aae0f2e
Iris Yoon <irishryoon@gmail.com>
2020-11-20 16:33:34 -0500

    added function for parsing statute info to get offense type

--- analyses/full_dockets/funcs_parse.py
+++ analyses/full_dockets/funcs_parse.py
@@ -2,6 +2,7 @@ import PyPDF2
 import re
 import numpy as np
 import pdfquery
+import pickle
 
 # Helper functions -----------------------------------------------------------
 def query_contains(page, searchTerm):
@@ -192,6 +193,64 @@ def get_magistrate(pdf, pages):
 
     return magistrate
 
+def get_offense_type(statute):
+    """
+    parses statute information and gets offense type 
+    """
+    # input: (list of strings) statute output of 'get_charges'
+    # output: (list of strings) offense type 
+    offense_type = []
+
+    # read offense category dictionary
+    with open('offense_category.pickle', 'rb') as handle:
+        offense_category = pickle.load(handle)
+
+    # find title and chapter numbers of offenses
+    for item in statute:
+        # remove whitespaces
+        statute_idx = item.replace("."," ")
+        statute_idx = statute_idx.replace("-"," ")
+        statute_idx = statute_idx.replace('§'," ")
+        statute_idx = statute_idx.split()
+        
+        # get title 
+        title = statute_idx[0]
+
+        # get chapter
+        # default chapter
+        chapter = statute_idx[1][:2]
+
+        # edge cases
+        if title == '0':
+            chapter = '0'
+        if title == '18':
+            if len(statute_idx[1]) == 4:
+                chapter = statute_idx[1][:2]
+            else:
+                #print(statute_idx[1])
+                chapter = statute_idx[1][:1]
+        elif title == '35':
+            chapter = statute_idx[1]
+        elif title == '75':
+            if int(statute_idx[1]) < 3731:
+                # Chapter 37 subchapter A
+                chapter = '1'
+            elif int(statute_idx[1]) < 3741:
+                # Chapter 37 subchapter B
+                chapter = '2'
+            elif int(statute_idx[1]) < 3800:
+                # Chapter 38 subchapter C
+                chapter = '3'
+            else:
+                chapter = statute_idx[1][:2]
+
+        # write exception handling for when the given idx cannot be found
+        if (int(title), int(chapter)) in offense_category.keys():
+            offense_type.append(offense_category[(int(title), int(chapter))])
+        else:
+            offense_type.append('NA')
+            print('Warning: could not parse statute title ' + title + ' chapter ' + chapter)
+    return offense_type
 
 def get_charges(pdf, pages):
     """ Return the list of charges and statutes """
@@ -221,4 +280,5 @@ def get_charges(pdf, pages):
         chargeList.extend(charges)
         statuteList.extend(statutes)
 
-    return chargeList, date, statuteList
+    offense_type = get_offense_type(statuteList)
+    return chargeList, date, statuteList, offense_type
